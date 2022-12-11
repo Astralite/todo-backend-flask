d1bdcee42c81ddc26b473a52321743a5ccd4e359
Astralite <astracodes@protonmail.com>
2022-12-11 20:42:29 +0800

    Add waitress server & python requirements file

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from app import app, db
 if __name__ == '__main__':
     try:
         print('Starting server')
-        app.run(debug=False)
+
+        # production server
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=3001)
+
+        # development server (Port 5000)
+        # app.run(debug=False)
     except e:
         print(e)
     finally:
